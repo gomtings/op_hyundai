e1cdc4997b82294c6b9c1519940c83cd640a3d94
sangwoo <tkddn4508@gmail.com>
2022-02-25 23:49:30 +0900

    LANE_CHANGE_SPEED DOWN
    * 차선 변경 속도 변경 60 -> 40
    * 차선변경 대기시간 1.0 -> 0.0

--- selfdrive/controls/lib/desire_helper.py
+++ selfdrive/controls/lib/desire_helper.py
@@ -3,13 +3,13 @@ from common.realtime import DT_MDL
 from selfdrive.config import Conversions as CV
 from common.params import Params
 
-AUTO_LCA_START_TIME = 1.0
+AUTO_LCA_START_TIME = 0.0
 
 LaneChangeState = log.LateralPlan.LaneChangeState
 LaneChangeDirection = log.LateralPlan.LaneChangeDirection
 
-LANE_CHANGE_SPEED_MIN = 60 * CV.KPH_TO_MS
-LANE_CHANGE_TIME_MAX = 10.
+LANE_CHANGE_SPEED_MIN = 40 * CV.KPH_TO_MS
+LANE_CHANGE_TIME_MAX = 30.
 
 DESIRES = {
   LaneChangeDirection.none: {
