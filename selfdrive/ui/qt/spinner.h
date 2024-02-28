4e0fa8380f54aabdde74dd5648bc3df5dab843bf
fallen8angel <75917889+fallen8angel@users.noreply.github.com>
2024-02-28 15:44:53 +0900

    Update spinner.h

--- selfdrive/ui/qt/spinner.h
+++ selfdrive/ui/qt/spinner.h
@@ -8,7 +8,7 @@
 #include <QWidget>
 
 constexpr int spinner_fps = 30;
-constexpr QSize spinner_size = QSize(360, 360);
+constexpr QSize spinner_size = QSize(1920, 1080);
 
 class TrackWidget : public QWidget  {
   Q_OBJECT
