6731cf91fd43672abb292055f31def0497c2370a
neokii <neokii@gmail.com>
2024-03-15 12:32:16 +0900

    more safely

--- selfdrive/controls/neokii/navi_controller.py
+++ selfdrive/controls/neokii/navi_controller.py
@@ -46,8 +46,7 @@ class NaviServer:
     broadcast.daemon = True
     broadcast.start()
 
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    subprocess.Popen(["./ngpsd"])
+    subprocess.Popen([os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ngpsd')])
 
     speed = Thread(target=self.speed_thread, args=[])
     speed.daemon = True
