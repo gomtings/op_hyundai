1354491b6be9bc1426b05f9f13848c80417eea82
leesangwoo <tkddn4508@naver.com>
2025-07-13 23:25:10 +0900

    apply changes 0713
    1. acados: relative rpath
    2. pigeond: power cycle on reinit
    3. bump opendbc
    4. ci: gate timeout on setup time
    5. raylib: Improve close settings x button
    6. fix(raylib): Show correct alert status
    7. fix(raylib): Correct icon for metric system toggle
    8. cabana: fix dark theme detection to work with system theme
    9. Fix stock ACC resume signal
    10. refactor(raylib): Use font file as FontWeight enum value
    11. refactor(raylib): Move widgets to the widgets folder
    12. Toyota: allow sport gear
    13. registration required to go onroad
    14. modeld: autodetect tinygrad backend
    15. cabana: Fix slider visual inconsistency by adjusting groove height
    16. [bot] Update translations
    17. [bot] Update Python packages
    18. Revert "modeld: autodetect tinygrad backend"
    19. selfdrived: rename temporary variable

--- opendbc_repo/opendbc/car/car.capnp
+++ opendbc_repo/opendbc/car/car.capnp
@@ -590,7 +590,6 @@ struct CarParams {
   }
 
   struct LateralTorqueTuning {
-    useSteeringAngle @0 :Bool;
     kp @1 :Float32;
     ki @2 :Float32;
     friction @3 :Float32;
@@ -598,6 +597,7 @@ struct CarParams {
     steeringAngleDeadzoneDeg @5 :Float32;
     latAccelFactor @6 :Float32;
     latAccelOffset @7 :Float32;
+    useSteeringAngleDEPRECATED @0 :Bool;
   }
 
   struct LongitudinalPIDTuning {
--- selfdrive/modeld/modeld.py
+++ selfdrive/modeld/modeld.py
@@ -19,6 +19,7 @@ import numpy as np
 import cereal.messaging as messaging
 from cereal import car, log
 from pathlib import Path
+from setproctitle import setproctitle
 from cereal.messaging import PubMaster, SubMaster
 from msgq.visionipc import VisionIpcClient, VisionStreamType, VisionBuf
 from opendbc.car.car_helpers import get_demo_car_params
@@ -28,6 +29,7 @@ from openpilot.common.filter_simple import FirstOrderFilter
 from openpilot.common.realtime import config_realtime_process, DT_MDL
 from openpilot.common.transformations.camera import DEVICE_CAMERAS
 from openpilot.common.transformations.model import get_warp_matrix
+from openpilot.system import sentry
 from openpilot.selfdrive.controls.lib.desire_helper import DesireHelper
 from openpilot.selfdrive.controls.lib.drive_helpers import get_accel_from_plan, smooth_value, get_curvature_from_plan
 from openpilot.selfdrive.modeld.parse_model_outputs import Parser
@@ -36,7 +38,6 @@ from openpilot.selfdrive.modeld.constants import ModelConstants, Plan
 from openpilot.selfdrive.modeld.models.commonmodel_pyx import DrivingModelFrame, CLContext
 
 from openpilot.selfdrive.controls.ntune import ntune_common_get, ntune_scc_get
-
 PROCESS_NAME = "selfdrive.modeld.modeld"
 SEND_RAW_PRED = os.getenv('SEND_RAW_PRED')
 
@@ -189,6 +190,9 @@ class ModelState:
 def main(demo=False):
   cloudlog.warning("modeld init")
 
+  sentry.set_tag("daemon", PROCESS_NAME)
+  cloudlog.bind(daemon=PROCESS_NAME)
+  setproctitle(PROCESS_NAME)
   if not USBGPU:
     # USB GPU currently saturates a core so can't do this yet,
     # also need to move the aux USB interrupts for good timings
@@ -376,4 +380,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     main(demo=args.demo)
   except KeyboardInterrupt:
-    cloudlog.warning("got SIGINT")
+    cloudlog.warning(f"child {PROCESS_NAME} got SIGINT")
+  except Exception:
+    sentry.capture_exception()
+    raise
