fc31f6447b17b183d3b011ec7c987dab2f94eb78
leesangwoo <tkddn4508@naver.com>
2025-08-10 22:36:53 +0900

    Update extra_cars.py

--- opendbc_repo/opendbc/car/extra_cars.py
+++ opendbc_repo/opendbc/car/extra_cars.py
@@ -37,7 +37,6 @@ class CAR(Platforms):
 
   EXTRA_HONDA = ExtraPlatformConfig(
     [
-      CommunityCarDocs("Honda Accord 2023-24", "All"),
       CommunityCarDocs("Acura Integra 2023-25", "All"),
       CommunityCarDocs("Acura MDX 2015-16", "Advance Package"),
       CommunityCarDocs("Acura MDX 2017-20", "All"),
