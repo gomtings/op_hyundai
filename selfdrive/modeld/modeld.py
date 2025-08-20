510d3af9882191e3a1e17f6eb19d6620ef792ef5
gomtings <tkddn4508@naver.com>
2025-08-20 14:00:26 +0900

    apply changes 0820
    1. [bot] Update Python packages
    2. bump panda
    3. feedbackd: remove lkas toggle for this release
    4. model parser: use check missing for mhp checks
    5. Honda: Adding support for Honda City
    6. format logreader
    7. update to latest userdata partition
    8. File sourcing: simplify return type
    9. File sourcing: Not all files are logs
    10. Reapply "File sourcing: Not all files are logs

--- selfdrive/modeld/modeld.py
+++ selfdrive/modeld/modeld.py
@@ -110,8 +110,6 @@ class ModelState:
       'desire': np.zeros((1, ModelConstants.INPUT_HISTORY_BUFFER_LEN, ModelConstants.DESIRE_LEN), dtype=np.float32),
       'traffic_convention': np.zeros((1, ModelConstants.TRAFFIC_CONVENTION_LEN), dtype=np.float32),
       'features_buffer': np.zeros((1, ModelConstants.INPUT_HISTORY_BUFFER_LEN,  ModelConstants.FEATURE_LEN), dtype=np.float32),
-      'lateral_control_params': np.zeros((1, ModelConstants.LATERAL_CONTROL_PARAMS_LEN), dtype=np.float32),
-      'prev_desired_curv': np.zeros((1, ModelConstants.INPUT_HISTORY_BUFFER_LEN, ModelConstants.PREV_DESIRED_CURV_LEN), dtype=np.float32),
     }
 
     # img buffers are managed in openCL transform code
