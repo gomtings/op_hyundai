cd381c614f6fa5351170136ee869539c93911ae1
gomtings <tkddn4508@naver.com>
2025-09-11 23:14:59 +0900

    Steering Assist warning clarification

--- selfdrive/modeld/modeld.py
+++ selfdrive/modeld/modeld.py
@@ -382,6 +382,7 @@ def main(demo=False):
                                      (ntune_scc_get('longActuatorDelay') + LONG_SMOOTH_SECONDS) + DT_MDL,
                                      v_ego)
       raw_action = get_action_from_model(model_output, prev_action, DT_MDL, long_delay + DT_MDL, v_ego)
+
       prev_action = action
       fill_model_msg(drivingdata_send, modelv2_send, model_output, action, raw_action,
                      publish_state, meta_main.frame_id, meta_extra.frame_id, frame_id,
