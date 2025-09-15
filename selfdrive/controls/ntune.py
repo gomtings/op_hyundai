84debc8775fddaa40d4504582e48180671d19ede
gomtings <tkddn4508@naver.com>
2025-09-15 13:07:50 +0900

    ntune basics Unit update

--- selfdrive/controls/ntune.py
+++ selfdrive/controls/ntune.py
@@ -193,9 +193,9 @@ class nTune():
 
     if self.checkValue("useSteeringAngle", 0., 1., 1.):
       updated = True
-    if self.checkValue("latAccelFactor", 0.5, 4.5, 2.7):
+    if self.checkValue("latAccelFactor", 0.5, 4.5, 3.1729):
       updated = True
-    if self.checkValue("friction", 0.0, 0.2, 0.08):
+    if self.checkValue("friction", 0.0, 0.2, 0.09601):
       updated = True
     if self.checkValue("angle_deadzone_v2", 0.0, 2.0, 0.0):
       updated = True
