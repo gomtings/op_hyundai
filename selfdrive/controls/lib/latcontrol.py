eae63bb7590b9a2a503323206b21fbe17cbedfd6
leesangwoo <tkddn4508@naver.com>
2025-10-15 01:15:55 +0900

    apply changes 1015

--- selfdrive/controls/lib/latcontrol.py
+++ selfdrive/controls/lib/latcontrol.py
@@ -1,13 +1,10 @@
 import numpy as np
 from abc import abstractmethod, ABC
 
-from openpilot.common.realtime import DT_CTRL
-
 
 class LatControl(ABC):
-  def __init__(self, CP, CI, dt=DT_CTRL):
+  def __init__(self, CP, CI, dt):
     self.dt = dt
-    self.sat_time_dt = 1.0 * dt
     self.sat_limit = CP.steerLimitTimer
     self.sat_time = 0.
     self.sat_check_min_speed = 10.
@@ -16,7 +13,7 @@ class LatControl(ABC):
     self.steer_max = 1.0
 
   @abstractmethod
-  def update(self, active, CS, VM, params, steer_limited_by_safety, desired_curvature, curvature_limited, lat_delay: float):
+  def update(self, active: bool, CS, VM, params, steer_limited_by_safety: bool, desired_curvature: float, curvature_limited: bool, lat_delay: float):
     pass
 
   def reset(self):
@@ -25,8 +22,8 @@ class LatControl(ABC):
   def _check_saturation(self, saturated, CS, steer_limited_by_safety, curvature_limited):
     # Saturated only if control output is not being limited by car torque/angle rate limits
     if (saturated or curvature_limited) and CS.vEgo > self.sat_check_min_speed and not steer_limited_by_safety and not CS.steeringPressed:
-      self.sat_time += self.sat_time_dt
+      self.sat_time += self.dt
     else:
-      self.sat_time -= self.sat_time_dt
+      self.sat_time -= self.dt
     self.sat_time = np.clip(self.sat_time, 0.0, self.sat_limit)
     return self.sat_time > (self.sat_limit - 1e-3)
