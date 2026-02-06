74e8a5faa0070d4583b461a40aecc2d11a2ebdf4
gomtings <tkddn4508@naver.com>
2026-02-06 10:02:14 +0900

    Revert "fix"
    This reverts commit b055c81963c108e5a838fe4a926e86c4eac7ce86.

--- selfdrive/controls/lib/drive_helpers.py
+++ selfdrive/controls/lib/drive_helpers.py
@@ -1,6 +1,8 @@
 import numpy as np
 from openpilot.common.constants import ACCELERATION_DUE_TO_GRAVITY
 from openpilot.common.realtime import DT_CTRL, DT_MDL
+from openpilot.system.version import get_build_metadata
+from openpilot.selfdrive.controls.ntune import ntune_common_get
 
 MIN_SPEED = 1.0
 CONTROL_N = 17
@@ -26,7 +28,7 @@ def clip_curvature(v_ego, prev_curvature, new_curvature, roll) -> tuple[float, b
   # This function respects ISO lateral jerk and acceleration limits + a max curvature
   v_ego = max(v_ego, MIN_SPEED)
   max_curvature_rate = MAX_LATERAL_JERK / (v_ego ** 2)  # inexact calculation, check https://github.com/commaai/openpilot/pull/24755
-  new_curvature = np.clip(new_curvature,
+  new_curvature = np.clip(new_curvature * ntune_common_get('pathFactor'),
                           prev_curvature - max_curvature_rate * DT_CTRL,
                           prev_curvature + max_curvature_rate * DT_CTRL)
 
@@ -52,7 +54,7 @@ def get_accel_from_plan(speeds, accels, t_idxs, action_t=DT_MDL, vEgoStopping=0.
     a_target = 0.0
   should_stop = (v_target < vEgoStopping and
                  v_target_1sec < vEgoStopping)
-  return a_target, should_stop
+  return a_target, should_stop, v_target
 
 def curv_from_psis(psi_target, psi_rate, vego, action_t):
   vego = np.clip(vego, MIN_SPEED, np.inf)
